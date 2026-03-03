cbe3b586c434cb67978c156d3393cb2c0a5bd5b6
yosi <stress0803@gmail.com>
2026-03-03 17:12:54 +0900

    correction

--- Occount-back-test-by-hyeonho/src/routes/item.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { itemController } from '../controllers/item.controller';
-import { authenticateToken } from '../middleware/auth';
-
-const router = Router();
-router.use(authenticateToken);
-router.get('/', itemController.getAllItems);
-
-export default router;
